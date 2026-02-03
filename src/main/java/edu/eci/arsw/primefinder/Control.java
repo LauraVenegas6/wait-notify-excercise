faac94ff42fb7117704272685fbaa435305e762d
LauraVenegas6 <aleja09venegas@gmail.com>
2026-02-03 11:04:02 -0500

    Añade monitor compartido (pauseLock) y condición paused para sincronizar pausa/reanudación entre Control y PrimeFinderThread Co-authored-by: Sergio Idarraga <sergio.idarraga-t@mail.escuelaing.edu.co>

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -13,6 +13,8 @@ public class Control extends Thread {
     private final static int NTHREADS = 3;
     private final static int MAXVALUE = 30000000;
     private final static int TMILISECONDS = 5000;
+    private final Object lock = new Object();
+    private volatile boolean paused = false;
 
     private final int NDATA = MAXVALUE / NTHREADS;
 
@@ -21,17 +23,24 @@ public class Control extends Thread {
     private Control() {
         super();
         this.pft = new  PrimeFinderThread[NTHREADS];
-
         int i;
         for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
+            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA, lock, this);
             pft[i] = elem;
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1, lock, this);
     }
     
     public static Control newControl() {
         return new Control();
+    }
+
+    public Object getPauseLock() {
+        return lock;
+    }
+
+    public boolean isPaused(){
+        return paused;
     }
 
     @Override
